fe0aee80e5ec802fb5006ebb9aeb015fa766a559
Tainan <tainanzappa_20@hotmail.com>
2022-02-11 14:54:47 -0300

    Initial commit

---
+++ aula04/ex007.js
@@ -0,0 +1,11 @@
+var vel = 65.5;
+
+
+
+if (vel > 60 ) {
+    console.log(`A velocidade do seu carro é ${vel}`)
+    console.log('Você ultrapassou a velocidade permitida. MULTADO')
+} 
+
+
+
---
+++ aula04/ex008.js
@@ -0,0 +1,7 @@
+var pais = "USA"
+
+if (pais == "Brasil") {
+    console.log("Você é Brasileiro!")
+} else {
+    console.log("Estrangeiro.")
+}
